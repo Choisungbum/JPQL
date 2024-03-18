deab2ae5b48118d05454ed5a4afebfce142c406f
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2024-03-18 22:48:21 +0900

    JPA - 섹션 10. 객체지향 쿼리언어 1 - 기본문법
    - 페이징API
    - 조인
    - 서브 쿼리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -16,14 +16,44 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
+            member.setTeam(team);
+
             em.persist(member);
 
             em.flush();
             em.clear();
+
+            // 조인
+//            String query = "select m from Member m inner join m.team t";
+            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+
+            // 페이징 API
+//            for (int i = 0; i < 100; i++) {
+//                Member member = new Member();
+//                member.setUsername("member" + i);
+//                member.setAge(i);
+//                em.persist(member);
+//            }
+//
+//            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+//                    .setFirstResult(0)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            System.out.println("result.size() = " + result.size());
+//            for (Member member1 : result) {
+//                System.out.println("member1 = " + member1);
+//            }
 
             // JPQL로 가져온 값은 영속성 관리가 된다.
 //            List<Member> result = em.createQuery("select m from Member m", Member.class)
@@ -42,12 +72,12 @@ public class JpaMain {
 //            System.out.println("age = " + result[1]);
 
             // new 타입으로 조회
-            List<MemberDTO> result =  em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("username = " + memberDTO.getUsername());
-            System.out.println("age = " + memberDTO.getAge());
+//            List<MemberDTO> result =  em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+//                    .getResultList();
+//
+//            MemberDTO memberDTO = result.get(0);
+//            System.out.println("username = " + memberDTO.getUsername());
+//            System.out.println("age = " + memberDTO.getAge());
 
 
             tx.commit();
